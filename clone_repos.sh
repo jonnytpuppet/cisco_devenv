d8219cda388134853f5ccbb220e398ae27fa5dcb
Hunter Haugen <hunter@puppetlabs.com>
2015-09-30 15:30:03 -0700

    Adding netdev stdlib

--- clone_repos.sh
+++ clone_repos.sh
@@ -3,3 +3,4 @@
 git clone https://github.com/cisco/cisco-network-node-utils
 git clone https://github.com/cisco/cisco-nxapi
 git clone https://github.com/cisco/cisco-network-puppet-module
+git clone https://github.com/puppetlabs/netdev_stdlib
