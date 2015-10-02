38b580f53ca90142429d1997f1cd89545096e784
Jonathan Tripathy <jt@puppetlabs.com>
2015-10-02 14:00:25 +0100

    Dev environment script fixes related to storage space issues on switch.

--- push_gems.sh
+++ push_gems.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
+ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
 vagrant ssh-config n9kv1 > files/vagrant.ssh-config
 cd cisco-nxapi
 gem build cisco_nxapi.gemspec
-scp -F ../files/vagrant.ssh-config *.gem n9kv1:/var/home/vagrant
+scp -F ../files/vagrant.ssh-config *.gem n9kv1:/nxos/tmp
 cd ../cisco-network-node-utils
 gem build cisco_node_utils.gemspec
-scp -F ../files/vagrant.ssh-config *.gem n9kv1:/var/home/vagrant
+scp -F ../files/vagrant.ssh-config *.gem n9kv1:/nxos/tmp
 cd ..
-ssh -F files/vagrant.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install *.gem'
+ssh -F files/vagrant.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install /nxos/tmp/*.gem'
--- push_module.sh
+++ push_module.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 echo "WARNING: This copies the module to the switch's /etc/puppetlabs/code/modules directory for switch-local development. It may cause undefined behavior when editing code and using pluginsync from the master"
+ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
 vagrant ssh-config n9kv1 > files/vagrant.ssh-config
-scp -r -F files/vagrant.ssh-config cisco-network-puppet-module n9kv1:~
-ssh -F files/vagrant.ssh-config n9kv1 'sudo mv cisco-network-puppet-module /etc/puppetlabs/code/modules'
+scp -r -F files/vagrant.ssh-config cisco-network-puppet-module n9kv1:/nxos/tmp
+ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /etc/puppetlabs/code/modules/cisco-network-puppet-module'
+ssh -F files/vagrant.ssh-config n9kv1 'rm -rf /nxos/tmp/cisco-network-puppet-module/.git'
+ssh -F files/vagrant.ssh-config n9kv1 'sudo mv /nxos/tmp/cisco-network-puppet-module /etc/puppetlabs/code/modules/'
