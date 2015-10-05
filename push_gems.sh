68cffbe7e14b69925f17e18c2bb4f2ae50f7be48
Hunter Haugen <hunter@puppetlabs.com>
2015-10-05 10:56:58 -0700

    Fix gem order

--- push_gems.sh
+++ push_gems.sh
@@ -7,4 +7,4 @@ cd ../cisco-network-node-utils
 gem build cisco_node_utils.gemspec
 scp -F ../files/switch.ssh-config *.gem n9kv1:/nxos/tmp
 cd ..
-ssh -F files/switch.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install /nxos/tmp/*.gem'
+ssh -F files/switch.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install /nxos/tmp/cisco_nxapi*.gem /nxos/tmp/cisco_node_utils*.gem'
