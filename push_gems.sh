a48d72be702f46f432c0452ca4d02f5441bf26b5
Hunter Haugen <hunter@puppetlabs.com>
2015-10-05 10:19:57 -0700

    push scripts use ip

--- push_gems.sh
+++ push_gems.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
-vagrant ssh-config n9kv1 > files/vagrant.ssh-config
+ssh -F files/switch.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
 cd cisco-nxapi
 gem build cisco_nxapi.gemspec
-scp -F ../files/vagrant.ssh-config *.gem n9kv1:/nxos/tmp
+scp -F ../files/switch.ssh-config *.gem n9kv1:/nxos/tmp
 cd ../cisco-network-node-utils
 gem build cisco_node_utils.gemspec
-scp -F ../files/vagrant.ssh-config *.gem n9kv1:/nxos/tmp
+scp -F ../files/switch.ssh-config *.gem n9kv1:/nxos/tmp
 cd ..
-ssh -F files/vagrant.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install /nxos/tmp/*.gem'
+ssh -F files/switch.ssh-config n9kv1 'sudo ip netns exec management /opt/puppetlabs/puppet/bin/gem install /nxos/tmp/*.gem'
--- push_netdev.sh
+++ push_netdev.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
-vagrant ssh-config n9kv1 > files/vagrant.ssh-config
-scp -r -F files/vagrant.ssh-config netdev_stdlib n9kv1:/nxos/tmp
-ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /etc/puppetlabs/code/modules/netdev_stdlib'
-ssh -F files/vagrant.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/netdev_stdlib/.git'
-ssh -F files/vagrant.ssh-config n9kv1 'sudo mv /nxos/tmp/netdev_stdlib /etc/puppetlabs/code/modules'
+ssh -F files/switch.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/*'
+scp -F files/switch.ssh-config -r netdev_stdlib n9kv1:/nxos/tmp
+ssh -F files/switch.ssh-config n9kv1 'sudo rm -rf /etc/puppetlabs/code/modules/netdev_stdlib'
+ssh -F files/switch.ssh-config n9kv1 'sudo rm -rf /nxos/tmp/netdev_stdlib/.git'
+ssh -F files/switch.ssh-config n9kv1 'sudo mv /nxos/tmp/netdev_stdlib /etc/puppetlabs/code/modules'
